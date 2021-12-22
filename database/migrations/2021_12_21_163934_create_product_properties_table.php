f0a67afb07c800a76f83287bf9f480fa4d7cec17
jalil seif <jalilseif@yahoo.com>
2021-12-22 09:23:50 +0330

    create migrations

---
+++ database/migrations/2021_12_21_163934_create_product_properties_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductPropertiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_properties', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('name', 150);
+            $table->char('value', 150);
+            $table->decimal('price', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_properties');
+    }
+}
